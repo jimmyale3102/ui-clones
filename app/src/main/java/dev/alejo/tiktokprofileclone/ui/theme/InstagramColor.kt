fff91bca2953bf60f3b959142fb428a7317950dc
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-03 12:53:26 +0700

    feat: Add resources for Instagram profile

---
+++ app/src/main/java/dev/alejo/tiktokprofileclone/ui/theme/InstagramColor.kt
@@ -0,0 +1,16 @@
+package dev.alejo.tiktokprofileclone.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WhiteDarker = Color(0xFFF9F9F9)
+val LightBlack = Color(0xFF293038)
+val DotNotificationColor = Color(0xFFFF000C)
+val ButtonBorderColor = Color(0x26FFFFFF)
+val NoteStartColor = Color(0xFF63676B)
+val NoteTextColor = Color(0xFFB1B9C4)
+val HighlightBorderColor = Color(0xFF5D646E)
+val NewHighlightBorderColor = Color(0xFF24292F)
+val NoteEndColor = LightBlack
+val IconColor = WhiteDarker
+val TextColor = WhiteDarker
+val ButtonColor = LightBlack
